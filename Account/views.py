d5b330c8b374df502dafa2c46ba2bc9bdf6050f1
root <shiroed1211@naver..com>
2018-08-25 01:30:05 +0900

    login/logout

---
+++ Account/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm,LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+from django.template import RequestContext
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('https://www.naver.com/')
+    else:
+        form = UserForm()
+        return render(request,'Account/adduser.html',{'form':form})
+# Create your views here.
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username,password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('https://www.naver.com/')
+        else:
+            return HttpResponse('로그인 실패')
+    else:
+        form = LoginForm()
+        return render(request,'Account/login.html',{'form':form})
