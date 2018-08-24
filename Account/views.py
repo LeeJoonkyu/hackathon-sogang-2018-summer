236812af88a2cbf4603c9236f58ac1ab238b4d06
root <shiroed1211@naver..com>
2018-08-25 08:25:46 +0900

    location

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import UserForm,LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate
@@ -12,6 +13,8 @@ def signup(request):
             new_user = User.objects.create_user(**form.cleaned_data)
             login(request, new_user)
             return redirect('board:post_list')
+        else:
+            return HttpResponseRedirect('/error/')
     else:
         form = UserForm()
         return render(request,'Account/adduser.html',{'form':form})
@@ -27,7 +30,11 @@ def signin(request):
             login(request,user)
             return redirect('board:post_list')
         else:
-            return HttpResponse('로그인 실패')
+            return HttpResponseRedirect('/esignin/')
     else:
         form = LoginForm()
         return render(request,'Account/login.html',{'form':form})
+def esignin(request):
+    return render(request,'Account/esignin.html')
+def error(request):
+    return render(request,'Account/error.html')
